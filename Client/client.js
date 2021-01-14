31a46d996a0efb78627d8e5104f88cc0d57583d2
Anthony Kuregian <AnthonyKuregian@yahoo.com>
2021-01-14 11:35:37 -0800

    Server-Client Stable

---
+++ Client/client.js
@@ -0,0 +1,9 @@
+const socket = require("socket.io-client")("http://192.168.1.171:3000")
+
+socket.on("connect", function() {
+  console.log("Succesfully Connected to Server");
+
+    socket.on("updateState", function(state){
+    console.log("Updated the State for all Clients to: " + state)
+  })
+})
